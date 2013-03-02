89036d91b27b387da7d1e47e0374624e1aaf6bc1
Jonas Gebhardt <jonas.gebhardt@gmail.com>
2013-03-01 20:28:41 -0500

    video loop and playback

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 
+$(document).on('ready', function(){
+  $('.slidebutton').on('click', function(e){
+    var v = $('.vid')[0];
+    if (!v.paused){
+      $(this).animate({top: 0});
+      v.pause();
+    } else {
+      $(this).animate({top: -150});
+      v.play();
+    }
+  });
+});
