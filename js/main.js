19f93ccb4dff9907c9103c43ea645e6ced3c3bd1
Sebon Koo <asder89@gmail.com>
2013-03-03 14:26:05 -0500

    SSSS

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 
 $(document).on('ready', function(){
-  $('.slidebutton').on('click', function(e){
-    var v = $('.vid')[0];
-    if (!v.paused){
-      $(this).animate({top: 0});
-      v.pause();
-    } else {
-      $(this).animate({top: -150});
-      v.play();
-    }
-  });
+	var video = $('.vid')[0];
+	var update_panel_clicked = false;
+	
+	$('.update_panel').on('click', function(e){
+		console.log("asdas");
+		if(!update_panel_clicked) {
+			$(this).animate({top: -260});
+			video.pause();
+			update_panel_clicked = true;
+		} else {
+			$(this).animate({top: 0});
+			video.play();
+			update_panel_clicked = false;
+		}
+		
+		
+	});
+
+
 });
