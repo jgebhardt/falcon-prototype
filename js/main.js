b729dacf9a0709278ce2db4e25c28c76eb3a65ab
Sebon Koo <asder89@gmail.com>
2013-03-05 17:38:38 -0500

    Frames

--- js/main.js
+++ js/main.js
@@ -4,87 +4,93 @@ $(document).on('ready', function(){
 	var video = $('.vid')[0];
 	var update_panel_clicked = false;
 	
-	$('.left').on('click', function(e) {
-		$('.left').animate({left: 0});
-		$('.right').animate({left: 700});
-	
-	});
-	
-	$('.right').on('click', function(e) {
-		$('.left').animate({left: -600});
-		$('.right').animate({left: 100});
-	});
-	
-	$('.text0').text("FALCONNN");
-	
-	setTimeout(function() {
-		disable_buttons();
-		render_state(state);
-	}, 1000);
-	
-	
 	$('.prev').on('click', function(e){
-		console.log(state);
 		if(state > 0) {
 			state--;
 			render_state(state);
+			
 		} 
 	
 	});
 	
 	
 	$('.next').on('click', function(e){
-		console.log(state);
-		if(state < 9) {
+		if(state < 8) {
 			state++;
 			render_state(state);
 		}
 	});
 	
+	
+	$('.text0').text("FALCON FALCON FALCON FALCON FALCON FALCON FALCON FALCON FALCON FALCON FALCON FALCON FALCON FALCON FALCON FALCON FALCON FALCON ");
+	
+	
+	
+	
+	setTimeout(function() {
+		$('.splash').fadeOut(2000,  function() {
+			render_state(state);
+		});
+	}, 2000);
+	
+	
+	
 
 	function render_state(state) {
+		disable_buttons();
+		console.log(state);
+	
 		if(state == 0) {
-			$('.left').animate({left:-300});
-			$('.right').animate({left:400});
+			$('.left').animate({left:-300}, function() {enable_buttons();});
+			$('.right').animate({left:400}, function() {enable_buttons();});
 		} else if(state == 1) {
 			// hide completely then show
 			$('.left').animate({left:-700}, function() {
-				$('.left').animate({left:0});
+				$('.left').animate({left:0}, function() {enable_buttons();});
 			});
 			
 			$('.right').animate({left:800}, function() {
-				$('.right').animate({left:700});
+				$('.right').animate({left:700}, function() {enable_buttons();});
 			});
 		
 			
 		} else if(state == 2) {
-			$('.left').animate({left: -600});
-			$('.right').animate({left: 100});
+			$('.left').animate({left: -600}, function() {enable_buttons();});
+			$('.right').animate({left: 100}, function() {enable_buttons();});
 		
 		} else if(state == 3) {
-			$('.left').animate({left: 0});
-			$('.right').animate({left: 700});
+			$('.left').animate({left: 0}, function() {enable_buttons();});
+			$('.right').animate({left: 700}, function() {enable_buttons();});
 		} else if(state == 4) {
-			$('.left').animate({left: -600});
-			$('.right').animate({left: 100});
+			$('.left').animate({left: -600}, function() {enable_buttons();});
+			$('.right').animate({left: 100}, function() {enable_buttons();});
 		
 		} else if(state == 5) {
-			$('.left').animate({left: 0});
-			$('.right').animate({left: 700});
+			$('.left').animate({left: 0}, function() {enable_buttons();});
+			$('.right').animate({left: 700}, function() {enable_buttons();});
 		} else if(state == 6) {
-			$('.left').animate({left: -600});
-			$('.right').animate({left: 100});
+			$('.left').animate({left: -600}, function() {enable_buttons();});
+			$('.right').animate({left: 100}, function() {enable_buttons();});
 		} else if(state == 7) {
-			$('.left').animate({left: 0});
-			$('.right').animate({left: 700});
+			$('.left').animate({left: 0}, function() {enable_buttons();});
+			$('.right').animate({left: 700}, function() {enable_buttons();});
 		} else if(state == 8) {
-			$('.left').animate({left: -600});
-			$('.right').animate({left: 100});
+			$('.left').animate({left: -600}, function() {enable_buttons();});
+			$('.right').animate({left: 100}, function() {enable_buttons();});
 		} else if(state == 9) {
-			$('.left').animate({left: 0});
-			$('.right').animate({left: 700});
+			$('.left').animate({left: 0}, function() {enable_buttons();});
+			$('.right').animate({left: 700}, function() {enable_buttons();});
 		}
 	}
 	
+	function disable_buttons() {
+		$('.prev').css('pointer-events', 'none');
+		$('.next').css('pointer-events', 'none');
+	}
+	
+	function enable_buttons() {
+		$('.prev').css('pointer-events', 'auto');
+		$('.next').css('pointer-events', 'auto');
+	}
 
 });
